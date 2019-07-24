150c5553712e3254ddc57e8c0e11fa7d54baf53d
Alexis Watson <47117571+alexiswatson@users.noreply.github.com>
2019-07-24 17:54:18 -0400

    Issue #1 by alexiswatson: Updated README.md and project metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Alexis Watson - Web Designer, Engineer, and Accessibility Professional`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Personal website for Alexis Watson, built using Gatsby, React, Prismic, and Netlify.`,
+    author: `@alexiswatson`,
   },
   plugins: [
     {
