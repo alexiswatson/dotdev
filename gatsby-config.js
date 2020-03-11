75d8dfc9082c470b62075c71611f940c5c0b96bd
Alexis Watson <47117571+alexiswatson@users.noreply.github.com>
2020-03-11 19:09:19 -0400

    Removed SSR, Service Worker boilerplate.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,21 +23,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-transformer-remark',
-    'gatsby-plugin-offline',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `assets/images/favicon-512.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-remove-serviceworker',
   ],
 }
